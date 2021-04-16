1debaa8fd34ea479d841d63da90117ef0cf3c13b
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2021-04-16 16:28:30 +0300

    Project added

---
+++ MultiplicationTable/Assets/Scripts/MultDisplay.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultDisplay : MonoBehaviour
+{
+    public GameObject itemSample;
+    public Transform itemStartT;
+    private MultItem[,] items;
+    public int x=0, y=0, mult=0;
+    public int inputx, inputy, oldX, oldY;
+
+    void Start()
+    {
+        oldX = inputx;
+        oldY = inputy;
+        items = new MultItem[x,y];
+    }
+
+    void Update()
+    {
+        if (oldX != inputx || oldY != inputy) {
+            SetXY(inputx,inputy);
+        }
+        oldX = inputx;
+        oldY = inputy;
+    }
+
+    public void SetXY(int newX, int newY){
+        //x = newX;
+        //y = newY;
+        int newMult = newX*newY;
+
+        MultItem[,] newItems = new MultItem[newX, newY];
+        print("setcall");
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                if (i<newX && j< newY){
+                    //print("x,y,newX,newY,i,j: "+x+" "+y+" "+newX+" "+newY+" "+i+" "+j);
+                    newItems[i,j] = items[i,j];
+                }
+                else{
+                    //print("Destroy: "+i.ToString()+" x"+j.ToString());
+                    Destroy(items[i,j].gameObject);
+                }
+            }
+        }
+
+        for (int i = 0; i < newX; i++)
+        {
+            for (int j = 0; j < newY; j++)
+            {
+                if (i<x && j<y){continue;}
+                newItems[i,j] = CreateItem(i,j);
+            }
+        }
+        
+        x = newX;
+        y = newY;
+        items = newItems;
+
+        /*
+        for (int i = 0; i < Mathf.Min(x, newX); i++)
+        {
+            bool createRow = false;
+            bool deleteRow = false;
+            if (newX<=i) {
+                deleteRow = true;
+            }
+            else if (x<=i){
+                createRow = true;
+            }
+            for (int j = 0; j < Mathf.Min(y, newY); j++)
+            {
+                if (createRow){
+                    //create y
+                    continue;
+                }
+                else if (deleteRow) {
+                    //delete y
+                    continue;
+                }
+                if (x>i && newX>i){
+                //transfer
+                }
+                else if (y>j) {
+                    //delete
+                    deleteRow = true;
+                }
+                else {
+                    //add
+                    createRow = true;
+                }
+            }
+        }
+        */
+    }
+
+    private MultItem CreateItem(int coX, int coY){
+        GameObject g = GameObject.Instantiate(itemSample, itemStartT.position, itemStartT.rotation);
+        MultItem item = g.GetComponent<MultItem>();
+        item.SetCoordinates(coX, coY);
+        return item;
+    }
+}
---
+++ MultiplicationTable/Assets/Scripts/MultItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultItem : MonoBehaviour
+{
+    private int x,y;
+
+    public void SetTarget(){
+        print("SetTarget call ("+gameObject.name+")");
+    }
+
+    public void SetTargetAndDestroy(){
+        SetTarget();
+        Destroy(gameObject, 3f);
+    } 
+
+    public void CheckCoordinates(int matrixX, int matrixY){
+        if (!(x < matrixX && y < matrixY)){
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetCoordinates(int coX, int coY){
+        x = coX;
+        y = coY;
+
+        transform.position = new Vector3(x*2, transform.position.y, y*2);
+    }
+}
